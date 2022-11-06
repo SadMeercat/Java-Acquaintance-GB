bf15bc5407851c6ad25dbb1c24deb942faf912d9
SadMeercat <igor9823@gmail.com>
2022-11-06 18:05:39 +0300

    fixed

--- семинар 3/Task1.java
+++ семинар 3/Task1.java
@@ -1,30 +1,40 @@
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 class Task1{
     public static boolean finded = false;
-    private static void Wave(Point start, Point finish, int[][] map){
+    private static void Wave(List<Point> points, Point finish, int[][] map){
+        List<Point> newPoints = new ArrayList<>();
         System.out.println(PrintData(map));
-        System.out.println(start.x + " " + start.y);
-        if(start.x == finish.x && start.y == finish.y){
-            finded = true;
-            return;
+        for (int i = 0; i < points.size(); i++) {
+            Point tmpPoint = points.get(i);
+            if(tmpPoint.x == finish.x && tmpPoint.y == finish.y){
+                finded = true;
+                break;
+            }
+            if(tmpPoint.y-1 >= 0 && map[tmpPoint.y-1][tmpPoint.x] == 0){//down
+                map[tmpPoint.y-1][tmpPoint.x] = map[tmpPoint.y][tmpPoint.x] + 1;
+                newPoints.add(new Point(tmpPoint.x, tmpPoint.y-1));
+                //Wave(new Point(start.x,start.y-1), finish, map);
+            }
+            if(tmpPoint.y+1 < map.length && map[tmpPoint.y+1][tmpPoint.x] == 0){//up
+                map[tmpPoint.y+1][tmpPoint.x] = map[tmpPoint.y][tmpPoint.x] + 1;
+                newPoints.add(new Point(tmpPoint.x, tmpPoint.y+1));
+                //Wave(new Point(start.x,start.y+1), finish, map);
+            }
+            if(tmpPoint.x-1 >= 0 && map[tmpPoint.y][tmpPoint.x-1] == 0){//left
+                map[tmpPoint.y][tmpPoint.x-1] = map[tmpPoint.y][tmpPoint.x] + 1;
+                newPoints.add(new Point(tmpPoint.x-1, tmpPoint.y));
+                //Wave(new Point(start.x-1,start.y), finish, map);
+            }
+            if(tmpPoint.x+1 < map[tmpPoint.y].length && map[tmpPoint.y][tmpPoint.x+1] == 0){//right
+                map[tmpPoint.y][tmpPoint.x+1] = map[tmpPoint.y][tmpPoint.x] + 1;
+                newPoints.add(new Point(tmpPoint.x+1, tmpPoint.y));
+                //Wave(new Point(start.x+1,start.y), finish, map);
+            }
         }
         if(!finded){
-            if(start.y-1 >= 0 && map[start.y-1][start.x] == 0){//down
-                map[start.y-1][start.x] = map[start.y][start.x] + 1;
-                Wave(new Point(start.x,start.y-1), finish, map);
-            }
-            if(start.y+1 < map.length && map[start.y+1][start.x] == 0){//up
-                map[start.y+1][start.x] = map[start.y][start.x] + 1;
-                Wave(new Point(start.x,start.y+1), finish, map);
-            }
-            if(start.x-1 >= 0 && map[start.y][start.x-1] == 0){//left
-                map[start.y][start.x-1] = map[start.y][start.x] + 1;
-                Wave(new Point(start.x-1,start.y), finish, map);
-            }
-            if(start.x+1 < map[start.y].length && map[start.y][start.x+1] == 0){//right
-                map[start.y][start.x+1] = map[start.y][start.x] + 1;
-                Wave(new Point(start.x+1,start.y), finish, map);
-            }
+            Wave(newPoints, finish, map);
         }
         else{
             return;
@@ -40,8 +50,10 @@ class Task1{
         };
         Point startPoint = new Point(2,0);
         Point endPoint = new Point(3,4);
+        List<Point> tmp = new ArrayList<>();
+        tmp.add(startPoint);
         map[startPoint.y][startPoint.x] = 1;
-        Wave(startPoint, endPoint, map);
+        Wave(tmp, endPoint, map);
     }
     public static String PrintData(int[][] data){
         String result = "";
